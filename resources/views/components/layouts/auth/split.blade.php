361cd49484cb33c557cfa58bb0f1117059809fb7
Herman <hermansyah_@live.com>
2026-01-06 10:14:20 +0700

    Update authentication layout to use new logo component and adjust logo size

--- resources/views/components/layouts/auth/split.blade.php
+++ resources/views/components/layouts/auth/split.blade.php
@@ -7,11 +7,14 @@
         <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
                 <div class="absolute inset-0 bg-neutral-900"></div>
+                @php
+                    $orgSettings = \App\Models\OrgSettings::getInstance();
+                @endphp
                 <a href="{{ route('dashboard') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
-                    <span class="flex h-10 w-10 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="me-2 h-7 fill-current text-white" />
+                    <span class="flex h-12 w-12 items-center justify-center rounded-md me-2">
+                        <x-auth-logo class="h-10 fill-current text-white" />
                     </span>
-                    {{ config('app.name', 'Laravel') }}
+                    {{ $orgSettings->short_name ?: config('app.name', 'Laravel') }}
                 </a>
 
                 @php
@@ -28,8 +31,8 @@
             <div class="w-full lg:p-8">
                 <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <a href="{{ route('dashboard') }}" class="z-20 flex flex-col items-center gap-2 font-medium lg:hidden" wire:navigate>
-                        <span class="flex h-9 w-9 items-center justify-center rounded-md">
-                            <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+                        <span class="flex h-16 w-16 items-center justify-center rounded-md">
+                            <x-auth-logo class="size-16 fill-current text-black dark:text-white" />
                         </span>
 
                         <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
